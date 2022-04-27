2112694cc1a60a75144d2be9f51eb6ea3e0e406d
ROBERTHP <ROBERTHP roberthpinchao@gmail.com>
2022-04-27 10:36:12 -0500

    string data, detection

---
+++ string_data.py
@@ -0,0 +1,10 @@
+# string data to represent text
+string_data= "Hello Everyone!!, 12345 $$// 'anything' \"double quotes\""
+
+print (string_data)
+print (len(string_data)) # get length
+#store numbers inside strings 
+x= 5
+y= "6"
+
+print(str(x)+ y)
---
+++ string_detection.py
@@ -0,0 +1,13 @@
+# string detection
+x= "34234hello"
+y= "121212"
+
+# using isnumeric 
+print(x.isnumeric()) #false
+print(y.isnumeric()) #true
+
+print(" hello\n Python developers")
+use_triple= """multi 
+line
+string""" 
+print(use_triple)
