0077b1cfa967597352728c6ccbb8cfa695a182b2
ROBERTHP <ROBERTHP roberthpinchao@gmail.com>
2022-04-27 08:40:41 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+# VAriable declaration and Assigment
+name="Edwin"
+num= 7
+age=30
+
+#Multi-assigment within a single statement
+ 
+name, num, age= "Dario", 7, 21
+
+#get the output 
+
+print(name)
+print(num)
+print(age)
+
+# output multi values
+print(name, num, age)
+print( "The player name is",name,", his age is", num,"and his number is", age)
