bb073eddc51e8c7a87c1680e732410fd2dca799c
ROBERTHP <ROBERTHP roberthpinchao@gmail.com>
2022-05-03 11:49:56 -0500

    numbers data

---
+++ numbers_data.py
@@ -0,0 +1,16 @@
+# number data 
+x=3
+y=5.12
+z=5 +2.12j
+
+# display the type of numbers
+print (type(x))
+print (type(y))
+print (type(z))
+
+# using random module
+import random as rand
+print(rand.random())
+print(rand.randint(1,100))
+print(rand.choice([30,40,50,60,70]))
+print(rand.sample([30,40,50,60,70],3))
